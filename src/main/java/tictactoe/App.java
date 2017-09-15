ec19aad805bcc90c27b4eaf06d8a48caf8f3fa87
Ezex360 <eigiachero@gmail.com>
2017-09-15 03:43:58 -0300

    Mustache working

--- src/main/java/tictactoe/App.java
+++ src/main/java/tictactoe/App.java
@@ -1,10 +1,22 @@
 package tictactoe;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
     	staticFileLocation("/public");
         get("/hello", (req, res) -> "Hello World");
+        Map map = new HashMap();
+        get("/play", (req, res) -> {
+            return new MustacheTemplateEngine().render(
+                new ModelAndView(map, "play.mustache")
+            );
+        });
     }
 }
