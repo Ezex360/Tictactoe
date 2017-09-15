b6709e73401ff1af220b1d2156dd7bfdadfc92d9
Ezex360 <eigiachero@gmail.com>
2017-09-15 03:05:09 -0300

    Initialize public

--- src/main/java/tictactoe/App.java
+++ src/main/java/tictactoe/App.java
@@ -4,6 +4,7 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+    	staticFileLocation("/public");
         get("/hello", (req, res) -> "Hello World");
     }
 }
