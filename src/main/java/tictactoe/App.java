cc0f0c2ef8ac529958c04afee3d250d884a519d8
Ezex360 <eigiachero@gmail.com>
2017-09-15 03:00:51 -0300

    Add run.sh and Test of Spark

--- src/main/java/tictactoe/App.java
+++ src/main/java/tictactoe/App.java
@@ -1,13 +1,9 @@
 package tictactoe;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
     }
 }
